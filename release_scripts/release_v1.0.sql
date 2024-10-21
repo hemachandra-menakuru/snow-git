fb40501c6662d0a5140f62c1b1c37ca9ebc12e85
Hemachandra Menakuru <90375082+hemachandra-menakuru@users.noreply.github.com>
2024-10-21 03:15:02 +0000

    Updated release sql file

--- release_scripts/release_v1.0.sql
+++ release_scripts/release_v1.0.sql
@@ -1,2 +1,5 @@
+
+
+USE DATABASE SNOWGIT;
 execute immediate from @snowgit/branches/"feature/test2"/silver/tables/customer.sql;
 execute immediate from @snowgit/branches/"feature/test2"/silver/tables/sales.sql;
