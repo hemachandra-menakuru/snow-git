d358f55ac8b5225ddd1ee30d421b4bc630668ef3
Hemachandra Menakuru <hemachandra@Hemachandras-MacBook-Pro-NEW.local>
2024-11-03 13:48:37 +1300

    Added release 3.0 script

--- release_scripts/master_release.sql
+++ release_scripts/master_release.sql
@@ -1,4 +1,4 @@
 USE DATABASE SNOWGIT;
 USE SCHEMA SILVER;
 
-EXECUTE IMMEDIATE FROM @snowgit/branches/"feature/test2"/release_scripts/release_v3.0.sql;
+EXECUTE IMMEDIATE FROM @snowgit/branches/"feature/test3"/release_scripts/release_v3.0.sql;
