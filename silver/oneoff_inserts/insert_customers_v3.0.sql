35e3ba7409a971de0a9119517800d12a83e937a4
Hemachandra Menakuru <90375082+hemachandra-menakuru@users.noreply.github.com>
2024-11-04 22:49:34 +0000

    updated cus table

---
+++ silver/oneoff_inserts/insert_customers_v3.0.sql
@@ -0,0 +1,7 @@
+INSERT INTO silver.customers (customer_id, customer_name, email, city, mobilenumber)
+VALUES 
+(3001, 'John Doe', 'john.doe@example.com', 'Auckland', '+64 21 123 4567'),
+(3002, 'Jane Smith', 'jane.smith@example.com', 'Wellington', '+64 27 987 6543'),
+(3003, 'Michael Brown', 'michael.brown@example.com', 'Christchurch', '+64 22 555 1234'),
+(3004, 'Emily Davis', 'emily.davis@example.com', 'Hamilton', '+64 20 111 2223'),
+(3005, 'David Lee', 'david.lee@example.com', 'Queenstown', '+64 25 777 8889');
