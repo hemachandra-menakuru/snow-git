35f59d91669c43cb9d1025de6ab59f73653c2bc8
Hemachandra Menakuru <90375082+hemachandra-menakuru@users.noreply.github.com>
2024-11-04 22:59:07 +0000

    updated insert

--- release_scripts/release_v3.0.sql
+++ release_scripts/release_v3.0.sql
@@ -3,5 +3,7 @@
 USE DATABASE SNOWGIT;
 USE SCHEMA SILVER;
 execute immediate from @snowgit/branches/"feature/test3"/silver/tables/customer.sql;
-execute immediate from @snowgit/branches/"feature/test3"/silver/oneoff_inserts/insert_customers_v3.0.sql;
 execute immediate from @snowgit/branches/"feature/test3"/silver/functions/last_day_of_month.sql;
+execute immediate from @snowgit/branches/"feature/test3"/silver/oneoff_inserts/insert_customers_v3.0.sql;
+execute immediate from @snowgit/branches/"feature/test3"/silver/oneoff_inserts/insert_sales_v1.0.sql;
+
--- silver/oneoff_inserts/insert_customers_v3.0.sql
+++ silver/oneoff_inserts/insert_customers_v3.0.sql
@@ -1,7 +1,9 @@
 INSERT INTO silver.customers (customer_id, customer_name, email, city, mobilenumber)
 VALUES 
-(3001, 'John Doe', 'john.doe@example.com', 'Auckland', '+64 21 123 4567'),
-(3002, 'Jane Smith', 'jane.smith@example.com', 'Wellington', '+64 27 987 6543'),
-(3003, 'Michael Brown', 'michael.brown@example.com', 'Christchurch', '+64 22 555 1234'),
-(3004, 'Emily Davis', 'emily.davis@example.com', 'Hamilton', '+64 20 111 2223'),
-(3005, 'David Lee', 'david.lee@example.com', 'Queenstown', '+64 25 777 8889');
+(3001, 'Virat Kohli', 'virat.kohli@rcb.in', 'Mumbai', '+91 98765 43210'),
+(3002, 'Steve Smith', 'steve.smith@cricketaustralia.com', 'Sydney', '+61 41234 56789'),
+(3003, 'Kane Williamson', 'kane.williamson@blackcaps.nz', 'Auckland', '+64 21 123 4567'),
+(3004, 'Babar Azam', 'babar.azam@pcb.com.pk', 'Lahore', '+92 301 234 5678'),
+(3005, 'Rohit Sharma', 'rohit.sharma@mbi.in', 'Mumbai', '+91 99876 54321'),
+(3006, 'AB de Villiers', 'ab.devilliers@cricket.co.za', 'Pretoria', '+27 76 789 0123')
+
