daa89ed96c5f02e2dcb96cc31de4e86667f9eb46
Hemachandra Menakuru <90375082+hemachandra-menakuru@users.noreply.github.com>
2024-11-04 23:39:00 +0000

    Updated release

--- silver/tables/customer.sql
+++ silver/tables/customer.sql
@@ -4,6 +4,6 @@ CREATE OR ALTER TABLE silver.customers (
     email VARCHAR(255),
     city VARCHAR(255),
     mobilenumber VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --added in feature/test4
-    is_active BOOLEAN DEFAULT TRUE --added in feature/test4
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, ---added in feature/test4
+    is_active BOOLEAN DEFAULT TRUE ---added in feature/test4
 );
