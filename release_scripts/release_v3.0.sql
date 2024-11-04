b0728a0b98c9ed296a139cc71d88052a5c74dda7
Hemachandra Menakuru <90375082+hemachandra-menakuru@users.noreply.github.com>
2024-11-04 23:11:33 +0000

    updated insert

--- release_scripts/release_v3.0.sql
+++ release_scripts/release_v3.0.sql
@@ -5,5 +5,4 @@ USE SCHEMA SILVER;
 execute immediate from @snowgit/branches/"feature/test3"/silver/tables/customer.sql;
 execute immediate from @snowgit/branches/"feature/test3"/silver/functions/last_day_of_month.sql;
 execute immediate from @snowgit/branches/"feature/test3"/silver/oneoff_inserts/insert_customers_v3.0.sql;
-execute immediate from @snowgit/branches/"feature/test3"/silver/oneoff_inserts/insert_sales_v1.0.sql;
 
