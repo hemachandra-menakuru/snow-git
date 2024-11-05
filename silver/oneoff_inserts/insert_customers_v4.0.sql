e6ceaa973b63518c9967f045e0e5ca57a55f14cb
Hemachandra Menakuru <90375082+hemachandra-menakuru@users.noreply.github.com>
2024-11-05 02:41:37 +0000

    Demo2

--- silver/oneoff_inserts/insert_customers_v4.0.sql
+++ silver/oneoff_inserts/insert_customers_v4.0.sql
@@ -6,4 +6,4 @@ VALUES
 (3019, 'Keshav Maharaj', 'keshav.maharaj@cricket.co.za', 'Durban', '+27 74 678 9012', '2024-01-09', true),
 (3012, 'Jos Buttler', 'jos.buttler@ecb.co.uk', 'Manchester', '+44 7700 900234', '2024-01-12', true),
 (1313, 'Travis Head', 'travis.head@cricketaustralia.com', 'Adelaide', '+61 41234 56791', '2024-01-13', true),
-(1314, 'Devon Conway', 'devon.conway@blackcaps.nz', 'Christchurch', '+64 21 345 6789', '2024-01-14', true);
+(1314, 'Devon Conway', 'devon.conway@blackcaps.nz', 'Christchurch', '+64 21 345 6789', '2024-01-19', true);
