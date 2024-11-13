a569e18787983ce15123b14e020f982299608af5
Hemachandra Menakuru <90375082+hemachandra-menakuru@users.noreply.github.com>
2024-11-13 21:27:41 +0000

    Demo Steve-2

--- silver/oneoff_inserts/insert_customers_v3.0.sql
+++ silver/oneoff_inserts/insert_customers_v3.0.sql
@@ -4,6 +4,6 @@ VALUES
 (3002, 'Steve Smith', 'steve.smith@cricketaustralia.com', 'Sydney', '+61 41234 56789'),
 (3003, 'Kane Williamson', 'kane.williamson@blackcaps.nz', 'Auckland', '+64 21 123 4567'),
 (3004, 'Babar Azam', 'babar.azam@pcb.com.pk', 'Lahore', '+92 301 234 5678'),
-(3005, 'Rohit Sharma', 'rohit.sharma@mbi.in', 'Mumbai', '+91 99876 54321'),
+(3005, 'Rohit Sharma', 'rohit.sharma@mbi.in', 'Wellington', '+91 99876 54321'),
 (3006, 'AB de Villiers', 'ab.devilliers@cricket.co.za', 'Pretoria', '+27 76 789 0134')
 
